f9e03750114e75da273da1c5d55c5e30e4f2fee0
Phillip Nott <48110738+Phillip-Nott@users.noreply.github.com>
2019-10-06 22:23:14 -0600

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,36 @@
+var myMap = L.map("map", {
+    center: [39.8283, -98.5795],
+    zoom: 5
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+//read data into map
+var url = "https://data.world/timothyrenner/ufo-sightings/workspace/file?filename=nuforc_reports.json";
+
+d3.json(url, function(response) {
+
+    console.log(response);
+
+    var heatArray = [];
+
+    for (var i = 0; i < response.length; i++) {
+        var location = response[i].location;
+
+        if (location) {
+            heatArray.push([location.coordinates[1], location.coordinates[0]]);
+        }
+    }
+
+    var heat = L.heatLayer(heatArray, {
+        radius: 20,
+        blur: 35
+    }).addTo(myMap);
+
+})
