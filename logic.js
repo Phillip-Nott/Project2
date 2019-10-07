eb8593800c5f2fde8dfc96c59b0fa6c55d2e5141
Phillip Nott <48110738+Phillip-Nott@users.noreply.github.com>
2019-10-06 23:59:48 -0600

    Add files via upload

--- logic.js
+++ logic.js
@@ -1,36 +1,34 @@
 var myMap = L.map("map", {
-    center: [39.8283, -98.5795],
-    zoom: 5
+  center: [39.8283, -98.5759],
+  zoom: 5
 });
 
-// Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
+  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
 }).addTo(myMap);
 
-//read data into map
-var url = "https://data.world/timothyrenner/ufo-sightings/workspace/file?filename=nuforc_reports.json";
+var url = "ufo_sighting_locations.csv";
 
-d3.json(url, function(response) {
+d3.csv(url, function(response) {
 
-    console.log(response);
+  console.log(response);
 
-    var heatArray = [];
+  var heatArray = [];
 
-    for (var i = 0; i < response.length; i++) {
-        var location = response[i].location;
+  for (var i = 0; i < response.length; i++) {
+    var location = response[i].location;
 
-        if (location) {
-            heatArray.push([location.coordinates[1], location.coordinates[0]]);
-        }
+    if (location) {
+      heatArray.push([location.coordinates[1], location.coordinates[0]]);
     }
+  }
 
-    var heat = L.heatLayer(heatArray, {
-        radius: 20,
-        blur: 35
-    }).addTo(myMap);
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(myMap);
 
-})
+});
